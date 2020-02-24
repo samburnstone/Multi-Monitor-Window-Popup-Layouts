817432c6ab578d435dea0787add0d144e5994319
Sam Burnstone <sburnstone@scottlogic.com>
2020-02-24 12:50:22 +0000

    Style alert using bootstrap alert

--- src/container/index.js
+++ src/container/index.js
@@ -4,6 +4,6 @@ import createContainerUI from "./createContainerUI";
 if (testPopupsEnabled()) {
   createContainerUI();
 } else {
-  document.writeln("<h1>Looks like your browser is blocking pop-ups.</h1>");
-  document.writeln("<p>Please enable them and then refresh the page.</p>");
+  const popupAlertEl = document.getElementById("popups-disabled-alert");
+  popupAlertEl.classList.remove("d-none");
 }
