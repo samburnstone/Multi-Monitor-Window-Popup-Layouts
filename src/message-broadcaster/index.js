c9864e851ba1773007891bf8e1407511d5dc312d
Sam Burnstone <sam.burnstone@gmail.com>
2020-02-28 15:43:05 +0000

    Add alert when using browser that doesn't support SharedWorkers
    COUGH Safari COUGH

--- src/message-broadcaster/index.js
+++ src/message-broadcaster/index.js
@@ -1,2 +1,8 @@
+if (!window.SharedWorker) {
+  alert(
+    "Looks like you're using a browser that doesn't support SharedWorkers.\n\nPlease try this in another browser."
+  );
+}
+
 export default () =>
   new SharedWorker("./messageBroadcaster", { type: "module" });
