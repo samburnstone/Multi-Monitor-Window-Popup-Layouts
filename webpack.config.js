54e2e9eed20c18724d558b09dd3eefb989774969
Sam Burnstone <sburnstone@scottlogic.com>
2020-03-09 17:09:12 +0000

    Move static files into public folder

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,7 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin([
-      { from: "./*.html" },
-      { from: "./assets", to: "assets" },
-      { from: "./manifest.json" },
+      { from: "./public" },
       { from: "./node_modules/bootstrap/dist/css/bootstrap.min.css" },
       { from: "./node_modules/bootstrap/dist/css/bootstrap.min.css.map" }
     ]),
