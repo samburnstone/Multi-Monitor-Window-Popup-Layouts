80a59f558cccabf9ea1a3e99e7c21805ed3fcaa3
Sam Burnstone <sburnstone@scottlogic.com>
2020-03-09 15:28:33 +0000

    Lint fixes

--- src/message-broadcaster/index.js
+++ src/message-broadcaster/index.js
@@ -6,6 +6,7 @@ if (!window.SharedWorker) {
   const hasSeenAlert = localStorage.getItem(POP_UP_SEEN_KEY) === "true";
 
   if (!hasSeenAlert) {
+    // eslint-disable-next-line no-alert
     alert(
       "Looks like you're using a browser that doesn't support SharedWorkers.\n\nThings will still work to some extent, but you may be better off using another browser."
     );
@@ -13,7 +14,7 @@ if (!window.SharedWorker) {
     localStorage.setItem(POP_UP_SEEN_KEY, "true");
   }
 
-  window.SharedWorker = function() {
+  window.SharedWorker = () => {
     return {
       port: {
         postMessage: () => {},
