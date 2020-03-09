4fb2e0108726b0e710f4e4d5519e96d93d99631d
Sam Burnstone <sburnstone@scottlogic.com>
2020-03-09 17:53:04 +0000

    Modify pop-up test to open in small new window

--- src/container/testPopupsEnabled.js
+++ src/container/testPopupsEnabled.js
@@ -1,5 +1,10 @@
 export default () => {
-  const testPopup = window.open("./popups-enabled.html", null);
+  // Hacky test to determine whether users have pop-ups enabled for this site.
+  const testPopup = window.open(
+    "./popups-enabled.html",
+    "test-popup",
+    "top=200,left=0,height=100,width=100"
+  );
   if (testPopup) {
     testPopup.close();
     return true;
