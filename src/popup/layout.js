d867156aa88fd203c8dacb38c5186ab638944f2a
Sam Burnstone <sburnstone@scottlogic.com>
2020-03-11 12:40:30 +0000

    WIP

--- src/popup/layout.js
+++ src/popup/layout.js
@@ -1,14 +1,19 @@
 import { createPopupLayoutChangeMessage } from "message-broadcaster";
 
 export default (id, layout, messageBroadcaster) => {
+  const isChrome = navigator.userAgent.search("Chrome") > 0;
+
   // Report the current layout every 0.5 seconds
   const startReportingLayout = () => {
     setInterval(() => {
       const currentLayout = {
         x: window.screenLeft,
         y: window.screenY,
-        width: window.innerWidth,
-        height: window.innerHeight
+        // Chrome does not respect dimensions supplied via window features when noopener is supplied, so we do some
+        // manual resizing and repositioning.
+        // To simplify things, use outer[Width|Height] when using Chrome as that's the value expected when using resizeTo
+        width: isChrome ? window.innerWidth : window.outerWidth,
+        height: isChrome ? window.innerHeight : window.outerHeight
       };
 
       const message = createPopupLayoutChangeMessage(id, currentLayout);
@@ -17,19 +22,11 @@ export default (id, layout, messageBroadcaster) => {
   };
 
   if (navigator.userAgent.search("Chrome") > 0) {
-    // Chrome does not respect dimensions supplied via window features, so we do a
-    // hacky user agent check to determine whether we need to do some resizing & repositioning
-
-    // resizeTo expects width and height in terms of "outerWidth" and "outerHeight".
-    // layout is stored in terms of pop-up's "innerWidth" and "innerHeight", so need to do
-    // some adjustments to ensure content area is of correct size
-    const widthDiff = window.outerWidth - window.innerWidth;
-    const heightDiff = window.outerHeight - window.innerHeight;
-
     // Layout params are sent in format "<x>,<y>,<width>,<height>" via the query string
-    window.resizeTo(layout[2] + widthDiff, layout[3] + heightDiff);
+    const [x, y, width, height] = layout;
+    window.resizeTo(width, height);
     // Need to move after resizing, otherwise y will always be 0 for some reason!
-    window.moveTo(layout[0], layout[1]);
+    window.moveTo(x, y);
   }
 
   startReportingLayout();
