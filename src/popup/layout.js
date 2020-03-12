16e6624462a3871c5367044ab8e89965ed2d3407
Sam Burnstone <sburnstone@scottlogic.com>
2020-03-12 09:48:37 +0000

    Use outer width and height when storing layout in Chromium browsers

--- src/popup/layout.js
+++ src/popup/layout.js
@@ -1,14 +1,21 @@
 import { createPopupLayoutChangeMessage } from "message-broadcaster";
 
 export default (id, layout, messageBroadcaster) => {
+  const isChromium = navigator.userAgent.search("Chrome") > 0;
+
   // Report the current layout every 0.5 seconds
   const startReportingLayout = () => {
     setInterval(() => {
       const currentLayout = {
         x: window.screenLeft,
         y: window.screenY,
-        width: window.innerWidth,
-        height: window.innerHeight
+        // Chrome does not respect dimensions supplied via window features, so we do a
+        // hacky user agent check to determine whether we need to do some resizing & repositioning
+
+        // resizeTo (used in Chromium browsers) expects width and height in terms of "outerWidth" and "outerHeight", whereas
+        // windowFeatures (used in Firefox) expects in terms of "innerWidth" and "innerHeight"
+        width: isChromium ? window.outerWidth : window.innerWidth,
+        height: isChromium ? window.outerHeight : window.innerHeight
       };
 
       const message = createPopupLayoutChangeMessage(id, currentLayout);
@@ -16,24 +23,9 @@ export default (id, layout, messageBroadcaster) => {
     }, 500);
   };
 
-  if (navigator.userAgent.search("Chrome") > 0) {
-    // Chrome does not respect dimensions supplied via window features, so we do a
-    // hacky user agent check to determine whether we need to do some resizing & repositioning
-
-    // resizeTo expects width and height in terms of "outerWidth" and "outerHeight".
-    // layout is stored in terms of pop-up's "innerWidth" and "innerHeight", so need to do
-    // some adjustments to ensure content area is of correct size
-
-    // Hack: Chrome sometimes reports outerHeight as being less than innerHeight.
-    // Only saw this when running as Desktop PWA, in which case we know the height of the top bar
-    const pwaTopBarHeight = 38;
-    const heightDiff = Math.max(
-      window.outerHeight - window.innerHeight,
-      pwaTopBarHeight
-    );
-
+  if (isChromium) {
     // Layout params are sent in format "<x>,<y>,<width>,<height>" via the query string
-    window.resizeTo(layout[2], layout[3] + heightDiff);
+    window.resizeTo(layout[2], layout[3]);
     // Need to move after resizing, otherwise y will always be 0 for some reason!
     window.moveTo(layout[0], layout[1]);
   }
